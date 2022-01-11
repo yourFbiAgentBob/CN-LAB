ae8b80341b3841b7e19547a4e4f82072d6e6d10e
Neal Kaul <71339849+yourFbiAgentBob@users.noreply.github.com>
2022-01-11 13:41:32 +0530

    Add files via upload

---
+++ Cycle2/Lab6/clientUDP.py
@@ -0,0 +1,11 @@
+from socket import *
+serverName = "127.0.0.1"
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+sentence = input("\nEnter file name:  ")
+clientSocket.sendto(bytes(sentence,"utf-8"),(serverName, serverPort))
+filecontents,serverAddress = clientSocket.recvfrom(2048)
+print ('\nReply from Server:\n')
+print (filecontents.decode("utf-8"))
+clientSocket.close()
+clientSocket.close()
